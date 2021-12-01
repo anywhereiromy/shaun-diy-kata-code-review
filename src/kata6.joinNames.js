fbda2520e3455680b295104b5792ca70a43dedc9
Shaun Emery <shaunsheep18@gmail.com>
2021-12-01 17:08:36 +0000

    Kata 6 complete

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,17 @@
-const joinNames = namesObj => {};
-
+const joinNames = namesObj => {;
+ let newString = ''
+ const  arrayLength = namesObj.length
+ for (let i = 0; i < arrayLength; i++) {
+     newString += namesObj[i].name;
+     console.log (newString);
+     if (i < arrayLength -2) {
+         newString += ', '
+     }
+     if (i === arrayLength -2) {
+        newString += ' & '
+     }
+     console.log(newString);
+ }
+ return newString;
+ }
 module.exports = joinNames;
