3a8869b42f1701add0760768d1975cbb86962fbb
Shaun Emery <shaunsheep18@gmail.com>
2021-12-01 10:28:33 +0000

    complete humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,14 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    if (number === 1){
+        return [1,15,15];
+    }
+    if (number === 2) {
+        return [2, 24, 24];
+    } else {
+        const catYears = 24 + (number-2)*4;
+        const dogYears = 24 + (number-2)*5;
+        return [number, catYears, dogYears];
+    }
+};
 
 module.exports = humanCatDogYears;
